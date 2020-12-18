c07992ec60a95964fd42398893cb284ee2ce222c
OQ <zfdok@126.com>
2020-12-18 11:28:33 +0800

    1218,去除冗余

--- routes/onenet.js
+++ routes/onenet.js
@@ -3,10 +3,6 @@ var request = require('request');
 
 router.prefix('/onenet')
 
-router.get('/', async (ctx, next) => {
-  ctx.body = "12312";
-})
-
 router.get('/get_project_info', async (ctx, next) => {
   let result = {
     code: -1
@@ -72,87 +68,6 @@ router.get('/get_user_project_info', async (ctx, next) => {
   ctx.body;
 }
 
-router.get('/get_user_devicelist_zx', async (ctx, next) => {
-  let result = {
-    code: -1
-  }
-  let username = ctx.request.query.user
-  let user_group_id;
-  //假装查了数据库,并返回了用户对应的分组ID
-  if (username == '熊爸') {
-    user_group_id = 'AYqdps'
-  }
-  return new Promise((resolve, reject) => {
-    request({
-      method: 'GET',
-      timeout: 5000,
-      url:
-        'http://openapi.heclouds.com/application',
-      qs: {
-        action: 'QueryDeviceList',
-        version: 1,
-        project_id: ctx.state.projectID1,
-        group_id: user_group_id,
-        product_id: ctx.state.projectID_zx,
-        limit: 100
-      },
-      headers: {
-        Authorization: ctx.state.userToken1
-      }
-    }, function (error, response, body) {
-      result.msg = response
-      result.code = 0;
-      ctx.body = result
-      resolve(next());
-    })
-  });//return
-}//async
-), //get
-  function () {
-    ctx.body;
-  }
-
-
-router.get('/get_user_devicelist_ly', async (ctx, next) => {
-  let result = {
-    code: -1
-  }
-  let username = ctx.request.query.user
-  let user_group_id;
-  //假装查了数据库,并返回了用户对应的分组ID
-  if (username == '熊爸') {
-    user_group_id = 'AYqdps'
-  }
-  return new Promise((resolve, reject) => {
-    request({
-      method: 'GET',
-      timeout: 5000,
-      url:
-        'http://openapi.heclouds.com/application',
-      qs: {
-        action: 'QueryDeviceList',
-        version: 1,
-        project_id: ctx.state.projectID1,
-        group_id: user_group_id,
-        product_id: ctx.state.projectID_ly,
-        limit: 100
-      },
-      headers: {
-        Authorization: ctx.state.userToken1
-      }
-    }, function (error, response, body) {
-      result.msg = response
-      result.code = 0;
-      ctx.body = result
-      resolve(next());
-    })
-  });//return
-}//async
-), //get
-  function () {
-    ctx.body;
-  }
-
 router.get('/get_devicedetail', async (ctx, next) => {
   let result = {
     code: -1
@@ -180,11 +95,12 @@ router.get('/get_devicedetail', async (ctx, next) => {
     case 'lcjzx':
       product_id = ctx.state.projectID_lcjzx
       break;
+    case 'ylbwx':
+      product_id = ctx.state.projectID_ylbwx
+      break;
     default:
       break;
   }
-  console.log(product_id);
-  console.log(device_name);
   //假装查了数据库,并返回了用户对应的分组ID
   if (username == '熊爸') {
     user_group_id = 'AYqdps'
@@ -233,6 +149,21 @@ router.get('/get_device', async (ctx, next) => {
     case 'ly':
       product_id = ctx.state.projectID_ly
       break;
+    case 'znbwx':
+      product_id = ctx.state.projectID_znbwx
+      break;
+    case 'llc':
+      product_id = ctx.state.projectID_llc
+      break;
+    case 'zhlk':
+      product_id = ctx.state.projectID_zhlk
+      break;
+    case 'lcjzx':
+      product_id = ctx.state.projectID_lcjzx
+      break;
+    case 'ylbwx':
+      product_id = ctx.state.projectID_ylbwx
+      break;
     default:
       break;
   }
@@ -269,13 +200,41 @@ router.get('/get_device', async (ctx, next) => {
     ctx.body;
   }
 
-
-router.get('/get_user_devicelist_znbwx', async (ctx, next) => {
+router.get('/get_user_devicelist', async (ctx, next) => {
   let result = {
     code: -1
   }
   let username = ctx.request.query.user
   let user_group_id;
+
+  let type = ctx.request.query.type
+  let product_id = ""
+  switch (type) {
+    case 'zx':
+      product_id = ctx.state.projectID_zx
+      break;
+    case 'ly':
+      product_id = ctx.state.projectID_ly
+      break;
+    case 'znbwx':
+      product_id = ctx.state.projectID_znbwx
+      break;
+    case 'llc':
+      product_id = ctx.state.projectID_llc
+      break;
+    case 'zhlk':
+      product_id = ctx.state.projectID_zhlk
+      break;
+    case 'lcjzx':
+      product_id = ctx.state.projectID_lcjzx
+      break;
+    case 'ylbwx':
+      product_id = ctx.state.projectID_ylbwx
+      break;
+    default:
+      break;
+  }
+
   //假装查了数据库,并返回了用户对应的分组ID
   if (username == '熊爸') {
     user_group_id = 'AYqdps'
@@ -291,7 +250,7 @@ router.get('/get_user_devicelist_znbwx', async (ctx, next) => {
         version: 1,
         project_id: ctx.state.projectID1,
         group_id: user_group_id,
-        product_id: ctx.state.projectID_znbwx,
+        product_id: product_id,
         limit: 100
       },
       headers: {
@@ -310,16 +269,61 @@ router.get('/get_user_devicelist_znbwx', async (ctx, next) => {
     ctx.body;
   }
 
-router.get('/get_user_devicelist_llc', async (ctx, next) => {
+router.get('/get_device_history', async (ctx, next) => {
   let result = {
     code: -1
   }
   let username = ctx.request.query.user
   let user_group_id;
+  let type = ctx.request.query.type
+  let device_name = ctx.request.query.device_name
+  let start_time = ctx.request.query.start_time
+  let end_time = ctx.request.query.end_time
+  let identifier = ctx.request.query.identifier
+  let offset = ctx.request.query.offset
+  let limit = ctx.request.query.limit
+  let product_id = ""
+  switch (type) {
+    case 'zx':
+      product_id = ctx.state.projectID_zx
+      break;
+    case 'ly':
+      product_id = ctx.state.projectID_ly
+      break;
+    case 'znbwx':
+      product_id = ctx.state.projectID_znbwx
+      break;
+    case 'llc':
+      product_id = ctx.state.projectID_llc
+      break;
+    case 'zhlk':
+      product_id = ctx.state.projectID_zhlk
+      break;
+    case 'lcjzx':
+      product_id = ctx.state.projectID_lcjzx
+      break;
+    case 'ylbwx':
+      product_id = ctx.state.projectID_ylbwx
+      break;
+    default:
+      break;
+  }
+
   //假装查了数据库,并返回了用户对应的分组ID
   if (username == '熊爸') {
     user_group_id = 'AYqdps'
   }
+  /**** */
+  console.log(ctx.state.projectID1);
+  console.log(product_id);
+  // console.log(device_name);
+  console.log(device_name);
+  console.log(identifier);
+  console.log(start_time);
+  console.log(end_time);
+  console.log(offset);
+  console.log(limit);
+
   return new Promise((resolve, reject) => {
     request({
       method: 'GET',
@@ -327,12 +331,15 @@ router.get('/get_user_devicelist_llc', async (ctx, next) => {
       url:
         'http://openapi.heclouds.com/application',
       qs: {
-        action: 'QueryDeviceList',
+        action: 'QueryDevicePropertyHistory',
         version: 1,
         project_id: ctx.state.projectID1,
-        group_id: user_group_id,
-        product_id: ctx.state.projectID_llc,
-        limit: 100
+        product_id: product_id,
+        device_name,
+        identifier,
+        start_time, end_time,
+        sort: "2", offset,
+        limit
       },
       headers: {
         Authorization: ctx.state.userToken1
@@ -349,85 +356,4 @@ router.get('/get_user_devicelist_llc', async (ctx, next) => {
   function () {
     ctx.body;
   }
-
-router.get('/get_user_devicelist_zhlk', async (ctx, next) => {
-  let result = {
-    code: -1
-  }
-  let username = ctx.request.query.user
-  let user_group_id;
-  //假装查了数据库,并返回了用户对应的分组ID
-  if (username == '熊爸') {
-    user_group_id = 'AYqdps'
-  }
-  return new Promise((resolve, reject) => {
-    request({
-      method: 'GET',
-      timeout: 5000,
-      url:
-        'http://openapi.heclouds.com/application',
-      qs: {
-        action: 'QueryDeviceList',
-        version: 1,
-        project_id: ctx.state.projectID1,
-        group_id: user_group_id,
-        product_id: ctx.state.projectID_zhlk,
-        limit: 100
-      },
-      headers: {
-        Authorization: ctx.state.userToken1
-      }
-    }, function (error, response, body) {
-      result.msg = response
-      result.code = 0;
-      ctx.body = result
-      resolve(next());
-    })
-  });//return
-}//async
-), //get
-  function () {
-    ctx.body;
-  }
-
-router.get('/get_user_devicelist_lcjzx', async (ctx, next) => {
-  let result = {
-    code: -1
-  }
-  let username = ctx.request.query.user
-  let user_group_id;
-  //假装查了数据库,并返回了用户对应的分组ID
-  if (username == '熊爸') {
-    user_group_id = 'AYqdps'
-  }
-  return new Promise((resolve, reject) => {
-    request({
-      method: 'GET',
-      timeout: 5000,
-      url:
-        'http://openapi.heclouds.com/application',
-      qs: {
-        action: 'QueryDeviceList',
-        version: 1,
-        project_id: ctx.state.projectID1,
-        group_id: user_group_id,
-        product_id: ctx.state.projectID_lcjzx,
-        limit: 100
-      },
-      headers: {
-        Authorization: ctx.state.userToken1
-      }
-    }, function (error, response, body) {
-      result.msg = response
-      result.code = 0;
-      ctx.body = result
-      resolve(next());
-    })
-  });//return
-}//async
-), //get
-  function () {
-    ctx.body;
-  }
-
 module.exports = router
